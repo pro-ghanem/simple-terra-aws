10ce75b3a8047e73101630c623924a51fa9013ef
root <root@192.168.1.50>
2020-03-15 07:18:02 +0200

    ifrst-commit

---
+++ provider.tf
@@ -0,0 +1,9 @@
+# Configure the AWS Provider
+provider "aws" {
+  region                  = "eu-central-1"
+#  access_key = "${AWS_ACCESS}"
+#  secret_key = "${AWS_SECRET}"
+  shared_credentials_file = "/Users/<USER_NAME>/.aws/credentials"
+  profile                 = "default"
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# terraform-test-app
+
+Edit your USER_NAME in provider.tf file 
+
+Efit yout ssh Key in instace.tf file 
